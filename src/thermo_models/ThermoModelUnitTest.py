49c28fa893033bd9e50dd954774d2b919e087857
Panwa Promtep <panwapromtep@Panwas-MacBook-Air.local>
2023-06-02 17:08:24 +0700

    added test for basic binary raoults

--- src/thermo_models/ThermoModelUnitTest.py
+++ src/thermo_models/ThermoModelUnitTest.py
@@ -4,19 +4,21 @@ from ThermodynamicModel import RaoultsLawModel
 class TestRaoultsLawModelBinary(unittest.TestCase):
     def setUp(self):
         self.component_vapor_pressure = [10.0, 100.0]  # Example vapor pressures for two components
-        self.model = RaoultsLawModel()
+        self.model = RaoultsLawModel(82)
 
     def test_convert_x_to_y(self):
         x = [0.2, 0.8]
         expected_y = [0.2 * 10/ 82, 0.8 * 100/82]
-        calculated_y = self.model.convert_x_to_y(x)
-        self.assertEqual(calculated_y, expected_y)
+        calculated_y = self.model.convert_x_to_y(x, [10,100])
+        for i in range(len(expected_y)):
+            self.assertAlmostEqual(calculated_y[i], expected_y[i])
 
     def test_convert_y_to_x(self):
         y = [0.2, 0.8]
-        expected_x = [1/1.4, 1 - 1/1.4]
-        calculated_x = self.model.convert_y_to_x(y)
-        self.assertEqual(calculated_x, expected_x)
+        expected_x = [0.4, 0.6]
+        calculated_x = self.model.convert_y_to_x(y, [41,109+1/3])
+        for i in range(len(expected_x)):
+            self.assertAlmostEqual(calculated_x[i], expected_x[i])
 
 if __name__ == '__main__':
     unittest.main()
--- src/thermo_models/ThermodynamicModel.py
+++ src/thermo_models/ThermodynamicModel.py
@@ -1,32 +1,74 @@
 class ThermodynamicModel:
-    def convert_x_to_y(self, x_i):
+    """
+    Base class representing a thermodynamic model.
+
+    This is an abstract base class that defines the interface for thermodynamic models.
+
+    Methods:
+        convert_x_to_y(x_array):
+            Convert liquid mole fraction to vapor mole fraction.
+
+        convert_y_to_x(y_array):
+            Convert vapor mole fraction to liquid mole fraction.
+    """
+    def convert_x_to_y(self, x_array):
         """
-        Computes the conversion from liquid mole fraction to vapor mole fraction 
+        Computes the conversion from liquid mole fraction to vapor mole fraction.
 
         Args:
-            x (int or float): liquid mole fraction of a component
+            x_array (list[float]): Liquid mole fraction of each component.
 
         Raises:
-            NotImplementedError: base class
+            NotImplementedError: Base class method.
         """
         raise NotImplementedError("Method convert_x_to_y not implemented in base class")
 
-    def convert_y_to_x(self, y_i):
+    def convert_y_to_x(self, y_array):
         """
-        Computes the conversion from vapor mole fraction to liquid mole fraction 
+        Computes the conversion from vapor mole fraction to liquid mole fraction.
 
         Args:
-            y (int or float): vapor mole fraction of a component
+            y_array (list[float]): Vapor mole fraction of each component.
 
         Raises:
-            NotImplementedError: base class
+            NotImplementedError: Base class method.
         """
         raise NotImplementedError("Method convert_y_to_x not implemented in base class")
 
+
 class RaoultsLawModel(ThermodynamicModel):
-    def __init__(self, Pressure_system):
-        self.P_sys = Pressure_system
+    """
+    A class representing a thermodynamic model based on Raoult's law.
+
+    This model calculates the conversion between liquid mole fraction and vapor mole fraction
+    based on Raoult's law, which assumes ideal behavior for the components in the mixture.
+
+    Args:
+        P_sys (float): The system pressure in units compatible with the vapor pressures.
+
+    Attributes:
+        P_sys (float): The system pressure used in the calculations.
+
+    Methods:
+        convert_x_to_y(x_array, P_sat_array):
+            Convert liquid mole fraction to vapor mole fraction based on Raoult's law.
+        convert_y_to_x(y_array, P_sat_array):
+            Convert vapor mole fraction to liquid mole fraction based on Raoult's law.
+    """
+    def __init__(self, P_sys):
+        self.P_sys = P_sys
     
-    def convert_x_to_y(self, *x_i, P_isat):
-        
+    def convert_x_to_y(self, x_array, P_sat_array):
+        y_array = []
+        for index,x in enumerate(x_array):
+            y_array.append(x*P_sat_array[index]/self.P_sys)
+        return y_array
+    
+    def convert_y_to_x(self, y_array, P_sat_array):
+        x_array = []
+        for index,y in enumerate(y_array):
+            x_array.append(y*self.P_sys/P_sat_array[index])
+        return x_array
+            
+            
         
