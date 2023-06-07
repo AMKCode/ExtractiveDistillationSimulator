0ed6cd9e60935f31774015b69b63b05b01c5d05a
panwapromtep <panwapromtep@gmail.com>
2023-06-07 15:36:20 +0700

    change from list to numpy array for raoultsmodel

--- src/thermo_models/RaoultsLawModel.py
+++ src/thermo_models/RaoultsLawModel.py
@@ -1,5 +1,5 @@
-from .ThermodynamicModel import ThermodynamicModel
-from ..utils.AntoineEquation import AntoineEquation
+import numpy as np
+from ThermodynamicModel import ThermodynamicModel  
 
 class RaoultsLawModel(ThermodynamicModel):
     """
@@ -20,17 +20,35 @@ class RaoultsLawModel(ThermodynamicModel):
         convert_y_to_x(y_array, P_sat_array):
             Convert vapor mole fraction to liquid mole fraction based on Raoult's law.
     """
+    
     def __init__(self, P_sys):
         self.P_sys = P_sys
     
-    def convert_x_to_y(self, x_array, P_sat_array):
-        y_array = []
-        for index,x in enumerate(x_array):
-            y_array.append(x*P_sat_array[index]/self.P_sys)
+    
+    def convert_x_to_y(self, x_array: np.ndarray, P_sat_array: np.ndarray):
+        """
+        Converts liquid mole fraction (x) to vapor mole fraction (y) based on Raoult's law.
+
+        Args:
+            x_array (np.ndarray): An array of liquid mole fractions for each component in the mixture.
+            P_sat_array (np.ndarray): An array of saturation pressures for each component in the mixture.
+
+        Returns:
+            np.ndarray: An array of vapor mole fractions for each component in the mixture.
+        """
+        y_array = x_array * P_sat_array / self.P_sys
         return y_array
     
-    def convert_y_to_x(self, y_array, P_sat_array):
-        x_array = []
-        for index,y in enumerate(y_array):
-            x_array.append(y*self.P_sys/P_sat_array[index])
+    def convert_y_to_x(self, y_array: np.ndarray, P_sat_array: np.ndarray):
+        """
+        Converts vapor mole fraction (y) to liquid mole fraction (x) based on Raoult's law.
+
+        Args:
+            y_array (np.ndarray): An array of vapor mole fractions for each component in the mixture.
+            P_sat_array (np.ndarray): An array of saturation pressures for each component in the mixture.
+
+        Returns:
+            np.ndarray: An array of liquid mole fractions for each component in the mixture.
+        """
+        x_array = y_array * self.P_sys / P_sat_array
         return x_array
