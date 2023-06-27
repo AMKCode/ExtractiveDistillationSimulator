4a9db160e1b87b190ba5e95e450075a7816273f6
Kellen Roddy <kroddy2@jhu.edu>
2023-06-26 21:33:37 -0400

    Wilson Unit Test

--- src/thermo_models/WilsonModel.py
+++ src/thermo_models/WilsonModel.py
@@ -27,15 +27,16 @@ class WilsonModel(VLEModel):
         self.num_comp = num_comp
         self.P_sys = P_sys
         self.Lambdas = Lambdas
+
+    def get_activity_coefficient(self, num_comp, Lambdas, x_):
+        #From Equation 15 in Prausnitz, the gamma values are computed
+        #This will work for any number of components
+
         #Assert that Lambdas[(i,i)] = 1
         for i in range(num_comp):
             if (Lambdas[(i,i)] != 1):
                 raise ValueError('Lambda Coefficients entered incorrectly')
-
-
-    def get_activity_coefficicent(self, num_comp, Lambdas, x_):
-        #From Equation 15 in Prausnitz, the gamma values are computed
-        #This will work for any number of components
+            
         gamma_list = []
         for k in range(num_comp):
             gamma_k = 1
@@ -53,9 +54,26 @@ class WilsonModel(VLEModel):
             gamma_list.append(gamma_k)
         return gamma_list
     
-
+if __name__=='__main__':
+    #Test a Ternary Mixture of Acetone, Methyl Acetate, Methanol
+    # Acetone = 0
+    # Methyl Acetate = 1
+    # Methanol = 2
+    # Lambda values from Prausnitz Table IV
+    P_sys = 1.0325
+    num_comp = 3
+    x_ = [0.3, 0.2, 0.5]
+    Lambdas = {
+        (0,0) : 1.0,
+        (0,1) : 0.5781,
+        (0,2) : 0.6917,
+        (1,0) : 1.3654,
+        (1,1) : 1.0,
+        (1,2) : 0.6370,
+        (2,0) : 0.7681,
+        (2,1) : 0.4871,
+        (2,2) : 1.0
+        }
     
-
-    
-
-        
+    Acetone_MethylAcetate_Methanol = WilsonModel(num_comp, P_sys, Lambdas)  
+    print(Acetone_MethylAcetate_Methanol.get_activity_coefficient(3, Lambdas, x_))
