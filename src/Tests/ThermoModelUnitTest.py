d45b074e5f6ba40ac1742dea5c766c3ba74496b3
panwapromtep <panwapromtep@gmail.com>
2023-06-28 11:14:16 -0400

    corrected indentation

--- src/Tests/ThermoModelUnitTest.py
+++ src/Tests/ThermoModelUnitTest.py
@@ -16,35 +16,35 @@ import os, sys
 import utils.AntoineEquation as AE
 import matplotlib.pyplot as plt 
 
-class TestRaoultsLawAntoinePlotting(unittest.TestCase):
-    def setUp(self) -> None:
-        # Antoine Parameters for benzene
-        Ben_A = 4.72583
-        Ben_B = 1660.652
-        Ben_C = -1.461
+# class TestRaoultsLawAntoinePlotting(unittest.TestCase):
+#     def setUp(self) -> None:
+#         # Antoine Parameters for benzene
+#         Ben_A = 4.72583
+#         Ben_B = 1660.652
+#         Ben_C = -1.461
 
-        # Antoine Parameters for toluene
-        Tol_A = 4.07827
-        Tol_B = 1343.943
-        Tol_C = -53.773
+#         # Antoine Parameters for toluene
+#         Tol_A = 4.07827
+#         Tol_B = 1343.943
+#         Tol_C = -53.773
         
-        P_sys = 1.0325
-        # Create Antoine equations for benzene and toluene
-        benzene_antoine = AE.AntoineEquation(Ben_A, Ben_B, Ben_C)
-        toluene_antoine = AE.AntoineEquation(Tol_A, Tol_B, Tol_C)
+#         P_sys = 1.0325
+#         # Create Antoine equations for benzene and toluene
+#         benzene_antoine = AE.AntoineEquation(Ben_A, Ben_B, Ben_C)
+#         toluene_antoine = AE.AntoineEquation(Tol_A, Tol_B, Tol_C)
 
-        # Create a Raoult's law object
-        self.TolBenSys = RaoultsLawModel(2,P_sys,[benzene_antoine, toluene_antoine])
+#         # Create a Raoult's law object
+#         self.TolBenSys = RaoultsLawModel(2,P_sys,[benzene_antoine, toluene_antoine])
 
-    def testPlot(self):
-        # Use Raoult's law to plot the Txy
-        self.TolBenSys.plot_binary_Txy(100,0)
+#     def testPlot(self):
+#         # Use Raoult's law to plot the Txy
+#         self.TolBenSys.plot_binary_Txy(100,0)
         
-    def test_Convert_ytox_from_convert_xtoy_output_binary_case(self):
-        solution = (self.TolBenSys.convert_x_to_y(np.array([0.5,0.5])))
-        y_array_sol = solution[:-1]
-        temp_sol = solution[-1]
-        assert(solution.all() == self.TolBenSys.convert_y_to_x(y_array=y_array_sol).all())
+#     def test_Convert_ytox_from_convert_xtoy_output_binary_case(self):
+#         solution = (self.TolBenSys.convert_x_to_y(np.array([0.5,0.5])))
+#         y_array_sol = solution[:-1]
+#         temp_sol = solution[-1]
+#         assert(solution.all() == self.TolBenSys.convert_y_to_x(y_array=y_array_sol).all())
 
 class TestWilsonModel(unittest.TestCase):
     #Test a Ternary Mixture of Acetone, Methyl Acetate, Methanol
@@ -91,19 +91,9 @@ class TestWilsonModel(unittest.TestCase):
         # Create a Wilson's Model object
         self.TernarySys = WilsonModel(num_comp,P_sys,Lambdas,[Acetate_antoine, MethylAcetate_antoine ,Methanol_antoine] )
 
-        def testPlot(self):
-            # Use Wilson Model to plot the Txy
-            self.TernarySys.plot_ternary_txy(100,0)
-            
-            
-
-
-
-        
-        
-
-
-    pass
+    def testPlot(self):
+        # Use Wilson Model to plot the Txy
+        self.TernarySys.plot_ternary_txy(100,0)
 
 if __name__ == '__main__':
     unittest.main()
