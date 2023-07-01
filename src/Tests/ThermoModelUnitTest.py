1ecd591e08b80001aa005c9c474d9c07d93bc73f
Kellen Roddy <kroddy2@jhu.edu>
2023-07-01 11:11:00 -0400

    Margules unit test

--- src/Tests/ThermoModelUnitTest.py
+++ src/Tests/ThermoModelUnitTest.py
@@ -12,6 +12,7 @@ sys.path.append(PROJECT_ROOT)
 
 from thermo_models.RaoultsLawModel import RaoultsLawModel
 from thermo_models.WilsonModel import WilsonModel
+from thermo_models.MargulesModel import MargulesModel
 import os, sys
 import utils.AntoineEquation as AE
 import matplotlib.pyplot as plt 
@@ -46,8 +47,50 @@ import matplotlib.pyplot as plt
 #         temp_sol = solution[-1]
 #         assert(solution.all() == self.TolBenSys.convert_y_to_x(y_array=y_array_sol).all())
 
+class TestMargulesModel(unittest.Testcase):
+    # Test a Binary Mixture of Benzene and Toluene
+    # Benzene = 1
+    # Toluene = 2
+    # Constants obtained from Kassmann, et al. (1986)
+    def setUp(self) -> None:
+        P_sys = 1.0325
+        num_comp = 2
+        A_ = {
+            (1,2) : 0.0092,
+            (2,1) : -0.0090
+        }
+
+        # Antoine Parameters for benzene
+        Ben_A = 4.72583
+        Ben_B = 1660.652
+        Ben_C = -1.461
+
+        #Antoine Parameters for toluene
+        Tol_A = 4.07827
+        Tol_B = 1343.943
+        Tol_C = -53.773
+        
+        # Create Antoine equations for benzene and toluene
+        benzene_antoine = AE.AntoineEquation(Ben_A, Ben_B, Ben_C)
+        toluene_antoine = AE.AntoineEquation(Tol_A, Tol_B, Tol_C)
+
+        #Create a Margules' Model Object
+        self.MargulesSys = MargulesModel(num_comp, P_sys, A_, [benzene_antoine, toluene_antoine])
+
+    def testPlot(self):
+        # Use Raoult's law to plot the Txy
+        self.MargulesSys.plot_binary_Txy(100,0)
+        
+    def test_Convert_ytox_from_convert_xtoy_output_binary_case(self):
+        solution = (self.MargulesSys.convert_x_to_y(np.array([0.5,0.5])))
+        y_array_sol = solution[:-1]
+        temp_sol = solution[-1]
+        assert(solution.all() == self.MargulesSys.convert_y_to_x(y_array=y_array_sol).all())
+
+
+
 class TestWilsonModel(unittest.TestCase):
-    #Test a Ternary Mixture of Acetone, Methyl Acetate, Methanol
+    # Test a Ternary Mixture of Acetone, Methyl Acetate, Methanol
     # Acetone = 1
     # Methyl Acetate = 2 (AKA Acetic Acid)
     # Methanol = 3
