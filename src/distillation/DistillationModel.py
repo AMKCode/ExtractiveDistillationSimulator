f43d36f7f0ea160cc46ca0129e27aa6a4e9dd69c
panwapromtep <panwapromtep@gmail.com>
2023-07-25 16:03:22 -0400

    renamed distillation demo to example 1

--- src/distillation/DistillationModel.py
+++ src/distillation/DistillationModel.py
@@ -152,15 +152,20 @@ class DistillationModel:
         return x0_values, y0_values
 
         
-    def plot_distil_binary(self, axs):
+    def plot_distil_binary(self, axs, pbar=None):
         if self.num_comp != 2:
-            raise ValueError("This method can only be used for binary distillation.") 
-        ax1, ax2, ax3 = axs
-        ax1.set_xlim([0,1]); ax2.set_xlim([0,1]); ax3.set_xlim([0,1]); 
-        ax1.set_ylim([0,1]); ax2.set_ylim([0,1]); ax3.set_ylim([0,1]) 
+            raise ValueError("This method can only be used for binary distillation.")
+            
+        plots, fixed_plots = axs
+        ax1, ax2, ax3 = plots
+        ax1_fixed, ax2_fixed, ax3_fixed = fixed_plots
         
+        # Set limits for all main plots
+        for ax in [ax1, ax2, ax3]:
+            ax.set_xlim([0,1])
+            ax.set_ylim([0,1])
         
-        x1_space = np.linspace(0, 1, 1000)
+        x1_space = np.linspace(0, 1, 100)
         x_array = np.column_stack([x1_space, 1 - x1_space])
         y_array, t_evaluated = [], []
         
@@ -168,73 +173,73 @@ class DistillationModel:
             solution = self.thermo_model.convert_x_to_y(x)[0]
             y_array.append(solution[:-1])
             t_evaluated.append(solution[-1])
+            if pbar:
+                pbar.update(1)  # Increment the progress bar
         y_array = np.array(y_array)
 
-        ax1.plot(x_array[:,0], y_array[:,0]); ax2.plot(x_array[:,0], y_array[:,0]); ax3.plot(x_array[:,0], y_array[:,0])
-        ax1.plot([0,1], [0,1], linestyle='dashed'); ax2.plot([0,1], [0,1], linestyle='dashed'); ax3.plot([0,1], [0,1], linestyle='dashed')
+        # Plotting main plots
+        for ax in [ax1, ax2, ax3]:
+            ax.plot(x_array[:,0], y_array[:,0])
+            ax.plot([0,1], [0,1], linestyle='dashed')
 
         y_r = self.rectifying_step_xtoy(x1_space)
-        y_VB = self.stripping_step_xtoy(x1_space)
+        y_s = self.stripping_step_xtoy(x1_space)
         
-   
-        op_color = 'yellow'
+        op_color = 'green'
         
-        for i in range(len(x1_space)):
-            if (abs((y_r[i]) - y_VB[i]) <= 0.001):    
-                if (y_r[i] < y_array[i,0]):           
-                    op_color = 'green'
-                else:
-                    op_color = 'red'   
-        '''
-        ## ADD POINTS TO X AXIS TO REPRESENT NUMBER OF EQUILIBRIA ##
-        N = 0
-        x1, x2, y1, y2 = self.xD[0], self.xD[0], self.xD[0], self.xD[0]
-        x_pts = []
-        while (x1 > self.xB[0]):
-            x_pts.append(x1)
-            N += 1
-            solution = self.thermo_model.convert_y_to_x([y1, 1-y1])
-            x2 = solution[0]
-            y2 = rectifying_step_xtoy(x2)
-            x1 = x2
-            y1 = y2 
-
-        x_zero = np.zeros(N)       
-
-        ax2.scatter(x_pts, x_zero)
-        '''
-
-        ax3.plot(x1_space, y_r, color = op_color); ax2.plot(x1_space, y_r, color = op_color)
-        ax3.plot(x1_space, y_VB, color = op_color); ax1.plot(x1_space, y_VB, color = op_color)
+        ax3.plot(x1_space, y_r, color = op_color)
+        ax2.plot(x1_space, y_r, color = op_color)
+        ax3.plot(x1_space, y_s, color = op_color)
+        ax1.plot(x1_space, y_s, color = op_color)
         
         x_r_0, y_r_0  = self.find_rect_fixedpoints_binary(n=10)
-        x_VB_0, y_VB_0 = self.find_strip_fixedpoints_binary(n=10)
+        x_s_0, y_s_0 = self.find_strip_fixedpoints_binary(n=10)
         
-        ax3.scatter(x_r_0,y_r_0, s=100, c="red"); ax2.scatter( x_r_0,y_r_0, s=100, c="red")
-        ax3.scatter(x_VB_0, y_VB_0, s=100, c="red"); ax1.scatter(x_VB_0, y_VB_0, s=100, c="red")
+        # Scatter plots
+        ax3.scatter(x_r_0, y_r_0, s=50, c="red")
+        ax2.scatter(x_r_0, y_r_0, s=50, c="red")
+        ax3.scatter(x_s_0, y_s_0, s=50, c="red")
+        ax1.scatter(x_s_0, y_s_0, s=50, c="red")
 
-        ax1.set_xlabel('$x_{1}$'); ax2.set_xlabel('$x_{1}$'); ax3.set_xlabel('$x_{1}$')
-        ax1.set_ylabel('$y_{1}$'); ax2.set_ylabel('$y_{1}$'); ax3.set_ylabel('$y_{1}$')
-        ax1.set_title("Equilibrium and Stripping Line")
-        ax2.set_title("Equilibrium and Rectifying Line")
-        ax3.set_title("Equilibrium and Operating Lines")
+        # Set labels and titles for secondary plots
+        ax1_fixed.set_xlabel('$x_{1}$')
+        ax2_fixed.set_xlabel('$x_{1}$')
+        ax3_fixed.set_xlabel('$x_{1}$')
 
-        ax1.set_aspect('equal', adjustable='box'); ax2.set_aspect('equal', adjustable='box'); ax3.set_aspect('equal', adjustable='box')
+        # Move the x1 label down for secondary plots (e.g., 0.02)
+        ax1_fixed.xaxis.set_label_coords(0.5, -0.05)
+        ax2_fixed.xaxis.set_label_coords(0.5, -0.05)
+        ax3_fixed.xaxis.set_label_coords(0.5, -0.05)
+
+        # Disable y-axis for secondary plots
+        ax1_fixed.yaxis.set_ticks([])
+        ax2_fixed.yaxis.set_ticks([])
+        ax3_fixed.yaxis.set_ticks([])
+        
+
+        # Set aspect for all plots
+        for ax in [ax1, ax2, ax3]:
+            ax.set_aspect('equal', adjustable='box')
+        
+        # Plot and format all fixed plots
+        for ax_fixed, x in zip([ax1_fixed, ax2_fixed, ax3_fixed], [x_s_0, x_r_0, x_r_0]):
+            ax_fixed.scatter(x, [0]*len(x), marker='x', color='black')
+            ax_fixed.spines['top'].set_visible(False)
+            ax_fixed.spines['right'].set_visible(False)
+            ax_fixed.spines['bottom'].set_visible(False)
+            ax_fixed.spines['left'].set_visible(False)
+            ax_fixed.axhline(0, color='black')  # y=0 line
+            ax_fixed.set_xlim([0,1])
+            ax_fixed.yaxis.set_ticks([])
+            ax_fixed.yaxis.set_ticklabels([])
+            
+        # Disable x-axis labels for the main plots
+        for ax in [ax1, ax2, ax3]:
+            ax.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+        
+        plt.setp(ax1.get_xticklabels(), visible=False)
+        axs[0, 0].set_title("Equilibrium and Stripping Line")
+        axs[0, 1].set_title("Equilibrium and Rectifying Line")
+        axs[0, 2].set_title("Equilibrium and Operating Lines")
+        
         return [ax1,ax2,ax3]
-
-        # def find_fixed_point_binary(self):
-    #     def fixed_pt_eqn(var):
-    #         # x_0, y_0, r_min
-    #         x_0 = var[0]
-    #         y_0 = var[1]
-    #         r_min = var[2]
-    #         q = var[3]
-    #         # Fidkowski and Malone eqn 2
-    #         s = ((r_min+q)*((self.xF[0]-self.xB[0])/(self.xD[0]-self.xF[0]))) + q - 1
-    #         return [y_0 - (((r_min/(r_min+1))*x_0)+((1/(r_min+1))*self.xD[0])), # Fidkowski and Malone eqn 3b
-    #                 x_0 - (((s/(s+1))*y_0)+((1/(s+1))*self.xB[0])), # Fidkowski and Malone eqn 4b
-    #                 y_0 - self.thermo_model.convert_x_to_y(np.array([x_0, 1-x_0]))[0][0], 
-    #                 q - ((s+1)*((self.xD[0]-self.xF[0])/(self.xD[0]-self.xB[0])))+(r_min*((self.xF[0]-self.xB[0])/(self.xD[0]-self.xB[0])))] # Fidkowski and Malone eqn 2
-    #     sol = fsolve(fixed_pt_eqn, [0.5, 0.5, 1, 1])
-    #     print(sol)
-    #     return sol
