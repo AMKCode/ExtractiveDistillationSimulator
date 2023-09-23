83ae21265c830e078b5563bebbb06eaba3513285
Kellen Roddy <kroddy2@jhu.edu>
2023-09-23 10:36:50 -0400

    Added n-comp Van Laar get_activity

--- src/thermo_models/VanLaarModel.py
+++ src/thermo_models/VanLaarModel.py
@@ -21,19 +21,52 @@ class VanLaarModel(VLEModel):
         convert_y_to_x(y, psat, A, B):
             Convert vapor mole fraction to liquid mole fraction based on Van Laar.
     """
-    def __init__(self, num_comp: int, P_sys: float, partial_pressure_eqs: AntoineEquationBase10, A, B):
+    def __init__(self, num_comp: int, P_sys: float, A_coeff:dict, partial_pressure_eqs: AntoineEquationBase10, A, B):
         super().__init__(num_comp, P_sys)
+        self.A_coeff = A_coeff
         self.partial_pressure_eqs = partial_pressure_eqs
         self.A = A
         self.B = B
         self.use_jacobian = False
 
-    def get_activity_coefficient(self, x_array, Temp = None):
-
-        gamma1 = np.exp(self.A/(np.power(1+((self.A*x_array[0])/(self.B*x_array[1])), 2)))
-        gamma2 = np.exp(self.B/(np.power(1+((self.B*x_array[1])/(self.A*x_array[0])), 2)))
-        
-        return np.array([gamma1, gamma2])
+    def get_activity_coefficient(self, x_array, Temp:float):
+        if (self.num_comp == 2):
+            gamma1 = np.exp(self.A/(np.power(1+((self.A*x_array[0])/(self.B*x_array[1])), 2)))
+            gamma2 = np.exp(self.B/(np.power(1+((self.B*x_array[1])/(self.A*x_array[0])), 2)))
+            
+            return np.array([gamma1, gamma2])
+        else:
+            #Assert that A_coeff[(i,i)] = 1
+            for i in range(1, self.num_comp+1):
+                if (self.A_coeff[(i,i)] != 0):
+                    raise ValueError('Lambda Coefficients entered incorrectly')
+                
+            gammas = []
+            z_array = [] # First compute the mole fractions 
+            for i in range(1, self.num_comp+1):
+                denom = 0
+                for j in range(1, self.num_comp+1): #summation over j term in denominator
+                    if (i == j): # Aii = 0
+                        denom += (x_array[j-1])  # "If Aji / Aij = 0 / 0, set Aji / Aij = 1" -- Knapp
+                    else:
+                        denom += (x_array[j-1] * self.A_coeff[(j,i)] / self.A_coeff[(i,j)])
+                z_array.append((x_array[i] / denom))
+            
+            for k in range(1, self.num_comp+1):
+                term1 = 0 # summation of Aki * zi
+                term2 = 0 # summation of Aki * zk * zi
+                term3 = 0 # The sum of sum of Aji * (Akj / Ajk) * zj * zi
+                for i in range(1, self.num_comp+1):
+                    term1 += (self.A_coeff[(k,i)] * z_array[i-1])
+                    term2 += (self.A_coeff[(k,i)] * z_array[k-1] * z_array[i-1])
+                for j in range(1, self.num_comp+1):
+                    for i in range(1, self.num_comp+1):
+                        if (j == k): # "If Aji / Aij = 0 / 0, set Aji / Aij = 1" -- Knapp
+                            term3 += (self.A_coeff[(j,i)] * z_array[j-1] * z_array[i-1])
+                        else:
+                            term3 += (self.A_coeff[(j,i)] * self.A_coeff[(k,j)] / self.A_coeff[(j,k)] * z_array[j-1] * z_array[i-1])
+                gammas.append(np.exp((term1 - term2 - term3)/Temp))
+            return gammas
 
     def get_vapor_pressure(self, Temp)->np.ndarray:
         """
